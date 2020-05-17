0819e63661983374354af99cce4799d041ca552a
Anna Strik <58706349+annastrik@users.noreply.github.com>
2020-05-17 14:08:36 -0700

    Strik/footer links API test update (#54)
    * FooterLinksTest update

--- TopHap/src/test/java/org/tophap/FooterLinksTest.java
+++ TopHap/src/test/java/org/tophap/FooterLinksTest.java
@@ -5,11 +5,14 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.tophap.api.ValidLinksApiTest;
 import org.tophap.runner.MultipleTest;
 import pages.HomePage;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,6 +38,13 @@ public class FooterLinksTest extends MultipleTest {
     @Test
     void FooterLinksHaveAPIResponse200() throws IOException {
 
-        ValidLinksApiTest.forEachFooterLinkAPIResponse(APIResponse -> assertEquals(HttpStatus.SC_OK, APIResponse), getDriver());
+        final List<WebElement> footerLinks = getDriver().findElements(By.cssSelector(".th-link"));
+
+        for (WebElement footerLink : footerLinks) {
+            if (!("Chat with Us".equals(footerLink.getText()) || "hello@tophap.com".equals(footerLink.getText()))) {
+                ValidLinksApiTest.forEachLinkAPIResponse(APIResponse ->
+                        assertEquals(HttpStatus.SC_OK, APIResponse), footerLink.getAttribute("href"));
+            }
+        }
     }
 }
